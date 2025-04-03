75901cd56d4fb0a7f497f73e1f672382f97bce76
Lucas Monteiro <lmontecosta@gmail.com>
2025-04-03 15:06:01 -0300

    feat: permite limpar as atualizações feitas na página

--- js/app.js
+++ js/app.js
@@ -1,47 +1,44 @@
-carrinho = [];
+let listaProdutos = document.getElementById('lista-produtos');
+let quantidade = document.getElementById('quantidade');
+let valorTotal = document.getElementById('valor-total');
 let valorInicial = 0;
-
-//pegar a informação do combobox de produtos;**
-//pegar a quantidade de itens definida no input;**
-//validar preenchimento dos inputs obrigatórios;**
-//multiplicar quantidade pelo valor do produto;**
-//armazenar no array de carrinho a descrição, o valor e a quantidade do produto agrupadamente;**
-//atualizar a lista de carrinho;*
-//totalizar o valor dos produtos no carrinho;
+let carrinho = [];
 
 function adicionar() {
     [nomeProduto, valorProduto] = document.getElementById('produto').value.split(' - R$');
-    let quantidade = parseInt(document.getElementById('quantidade').value);
-    let totalProduto = quantidade * parseInt(valorProduto);
+    let valorQuantidade = parseInt(quantidade.value);
+    let totalProduto = valorQuantidade * parseInt(valorProduto);
     
-    if (isNaN(quantidade) || quantidade <= 0) {
+    if (isNaN(valorQuantidade) || valorQuantidade <= 0) {
         alert(`Você precisa informar uma quantidade válida. Tente novamente.`);
         return;
     }
     
-    carrinho.push(`${quantidade}x ${nomeProduto} R$ ${totalProduto}`);
-
-    atualizarCarrinho(quantidade, nomeProduto, totalProduto);    
+    carrinho.push(`${valorQuantidade}x ${nomeProduto} R$ ${totalProduto}`);
+    
+    adicionarProdutoCarrinho(valorQuantidade, nomeProduto, totalProduto);    
     totalizar(totalProduto);
 }
 
-function atualizarCarrinho(quantidade, descricao, valor) {    
-    let listaProdutos = document.getElementById('lista-produtos');
+function adicionarProdutoCarrinho(quantidade, descricao, valor) {    
     let novoProduto = document.createElement('section');
-
+    
     novoProduto.classList.add('carrinho__produtos__produto');
     novoProduto.innerHTML = `<span class="texto-azul">${quantidade} x</span> ${descricao} <span class="texto-azul"> R$${valor}</span>`;
     listaProdutos.appendChild(novoProduto);
 }
 
 function totalizar(valorAdicionado) {
-    let valorTotal = document.getElementById('valor-total');
     let somaTotal = valorInicial + valorAdicionado;
-    valorInicial = somaTotal
 
+    valorInicial = somaTotal
     valorTotal.innerHTML = `R$ ${somaTotal}`;
 }
 
 function limpar() {
-
+    valorInicial = 0
+    valorTotal.innerHTML = `R$ ${valorInicial}`;
+    carrinho = [];
+    quantidade.value = '';
+    listaProdutos.innerHTML = '';
 }
