9d9346fa66f5aab8d81517574fd0834bbe085a5b
Lucas Monteiro <lmontecosta@gmail.com>
2025-04-03 13:25:05 -0300

    feat: adiciona função de exibir lista de compra

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ carrinho = [];
 //validar preenchimento dos inputs obrigatórios;**
 //multiplicar quantidade pelo valor do produto;**
 //armazenar no array de carrinho a descrição, o valor e a quantidade do produto agrupadamente;**
-//atualizar a lista de carrinho;
+//atualizar a lista de carrinho;*
 //totalizar o valor dos produtos no carrinho;
 
 function adicionar() {
@@ -16,8 +16,19 @@ function adicionar() {
         return;
     }
     totalProduto = quantidade * parseInt(valorProduto);
-    carrinho.push(`${quantidade}x ${nomeProduto} R$ ${totalProduto}`)
-    console.log(carrinho);
+    
+    carrinho.push(`${quantidade}x ${nomeProduto} R$ ${totalProduto}`);
+
+    atualizarCarrinho(quantidade, nomeProduto, totalProduto);
+}
+
+function atualizarCarrinho(quantidade, descricao, valor) {    
+    let listaProdutos = document.getElementById('lista-produtos');
+    let novoProduto = document.createElement('section');
+
+    novoProduto.classList.add('carrinho__produtos__produto');
+    novoProduto.innerHTML = `<span class="texto-azul">${quantidade}</span> ${descricao} <span class="texto-azul"> R$${valor}</span>`;
+    listaProdutos.appendChild(novoProduto);
 }
 
 function limpar() {
